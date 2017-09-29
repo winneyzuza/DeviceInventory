3acc57f143b7bf9c621ebcac7390ae9e4ee03245
Winney <thammarath014@gmail.com>
2017-09-29 15:46:34 +0700

    set config

--- src/conf/Configuration.java
+++ src/conf/Configuration.java
@@ -4,6 +4,5 @@ public class Configuration {
 	public static final String fileConfiguration = "configuration.property";
 	public static final String pathDEV = "C:\\temp\\";
 	public static final String pathPROD = "/apps/deviceInventory/";
-	
-	
+
 }
