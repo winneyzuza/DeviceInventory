21c3ffc005f4d4d343221cd2a37408566ef6179d
Winney <thammarath014@gmail.com>
2017-09-29 15:46:05 +0700

    call from main

--- src/app/Device.java
+++ src/app/Device.java
@@ -7,8 +7,11 @@ public class Device {
 	public static void main(String[] args) {
 		try {
 			ReadWriteExcelFile rd = new ReadWriteExcelFile();
-			rd.saveReportToDB(true);
-			rd.saveReportToDB(false);
+			//rd.saveReportToDB(true);
+			//rd.saveReportToDB(false);		
+			rd.writeDeviceReport("0");
+
+			
 		}catch(Exception ex) {
 			ex.printStackTrace();
 		}
