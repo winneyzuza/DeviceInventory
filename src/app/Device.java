8563090250c138a40bcf5ce1cb1408597f9ac93e
Winney <thammarath014@gmail.com>
2017-10-03 10:29:24 +0700

    modified main class

--- src/app/Device.java
+++ src/app/Device.java
@@ -7,10 +7,37 @@ public class Device {
 	public static void main(String[] args) {
 		try {
 			ReadWriteExcelFile rd = new ReadWriteExcelFile();
-			//rd.saveReportToDB(true);
-			//rd.saveReportToDB(false);		
+			// BASE LINE
+			String fileBaseLineToDB = rd.getInfoByFileConfig("fileBaseLine");
+			rd.readFileAndPutBaseLineToDB(fileBaseLineToDB);
+			
+			// Agency Index
+			String fileAgencyToDB = rd.getInfoByFileConfig("fileAgency");
+			rd.readFileAndPutAgencyToDB(fileAgencyToDB);
+			
+			// Log Stat
+			rd.RemoveTableDB("logstats"); // no need to delete every day.
+			String fileLogStatsToDB = rd.getInfoByFileConfig("fileLogStats");
+			rd.readFileAndPutLogStatToDB(fileLogStatsToDB);
+			
+			// List Delete
+			String fileListDel = rd.getInfoByFileConfig("fileListDelete");
+			rd.readFileAndPutListDeleteToDB(fileListDel);
+			
+			//clear table deviceinventoryreport
+			rd.RemoveTableDB("deviceinventoryreport");
+			
+			// save to table deviceinventoryreport
+			rd.saveReportToDB(true);
+			rd.saveReportToDB(false);
+			
+			//remove list from table listdelete
+			rd.RemoveListDeleteFromReportDB();
+			
+			//write csv file
+			rd.writeDeviceReport("1");
 			rd.writeDeviceReport("0");
-
+			rd.writeDeviceReport("ALL");
 			
 		}catch(Exception ex) {
 			ex.printStackTrace();
